8583f7b2fdc1c4d319788296e86c08fba6fff48e
Lyubomyr Rudko <lrudko@shutterstock.com>
2025-02-05 00:16:10 +0200

    added subtract test

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
+
+def subtract(x, y):
+    """subtract y from x and return result."""
+    return x - y
